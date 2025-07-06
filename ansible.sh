1fab1d59c5a7ce77fa87fc2d8bf9627ea667665c
shuja-git <shuja.devops@gmail.com>
2025-07-06 06:18:00 +0530

    uncommented

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-env >/tmp/env
-#ansible-playbook -i $component-dev.shujadevops.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=d$env -e role_name=$component expense.yml
+#env >/tmp/env
+ansible-playbook -i $component-dev.shujadevops.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=d$env -e role_name=$component expense.yml
