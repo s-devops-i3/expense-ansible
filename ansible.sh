91e9354ee3ac67f2495e329e86f514dd0e9d2e8d
shuja-git <shuja.devops@gmail.com>
2025-07-06 15:17:17 +0530

    retriving secrets using ansible

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 #env >/tmp/env
-ansible-playbook get-secrets.yml -e vault_token=$vault_token
+ansible-playbook get-secrets.yml -e vault_token=$vault_token  -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.shujadevops.shop,  -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
