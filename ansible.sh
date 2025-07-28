7db6961493ca47101690c679eadc4aceb77d0bdb
shuja-git <shuja.devops@gmail.com>
2025-07-28 13:22:39 +0530

    testing get secrets

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,6 @@
 component=$1
 #env >/tmp/env
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+exit 1
 ansible-playbook -i $component-$env.shujadevops.shop,  -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
 rm -rf ~/secrets.json ~/app.json
