9bafa25a6547f1afa9625baa30d2019d2f868f3f
shuja-git <shuja.devops@gmail.com>
2025-07-28 14:39:32 +0530

    home path added

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,4 @@ component=$1
 rm -rf ~/*.json
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.shujadevops.shop,  -e env=$env -e role_name=$component expense.yml -e '@~/final.json'
-rm -rf ~/secrets.json ~/app.json
+rm -rf ~/*.json
